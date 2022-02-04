e48b551bd43f146178fd78788d2255ecdd10ceb1
zahidraza <md-zahid.raza@outlook.com>
2022-02-04 10:04:21 +0530

    v1.0.3 - release

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+import * as Application from "expo-application";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Version 1</Text>
+      <Text>Version {Application.nativeApplicationVersion}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +15,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
