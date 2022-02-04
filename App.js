a36a9bb1745b9a5f7d116205f2197e43d3060b35
zahidraza <md-zahid.raza@outlook.com>
2022-02-04 11:31:10 +0530

    v1.0.9 - release

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Version {Application.nativeApplicationVersion}</Text>
-      <Text>Version 1.0.7</Text>
+      <Text>Version 1.0.9</Text>
       <StatusBar style="auto" />
     </View>
   );
