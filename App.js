b93cacc51f6711fbbe10d8d5953a7f32c2aaf92d
zahidraza <md-zahid.raza@outlook.com>
2022-02-04 11:42:51 +0530

    v1.0.11 - release

--- App.js
+++ App.js
@@ -1,13 +1,39 @@
+import * as React from "react";
+
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
 import * as Application from "expo-application";
 
+import * as Updates from "expo-updates";
+
 export default function App() {
+  const [isUpdateAvailable, setIsUpdateAvailable] = React.useState(false);
+  const [error, setError] = React.useState(null);
+
+  React.useEffect(() => {
+    const checkUpdateAsync = async () => {
+      try {
+        const result = await Updates.checkForUpdateAsync();
+        setIsUpdateAvailable(result.isAvailable);
+      } catch (error) {
+        if (typeof error === "string") {
+          setError(error);
+        } else {
+          setError(JSON.stringify(error));
+        }
+      }
+    };
+
+    checkUpdateAsync();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Version {Application.nativeApplicationVersion}</Text>
-      <Text>Version 1.0.9</Text>
+      <Text>Version 1.0.11</Text>
+      <Text>Update Available = {isUpdateAvailable ? "Yes" : "No"}</Text>
+      <Text>Error = {error}</Text>
       <StatusBar style="auto" />
     </View>
   );
